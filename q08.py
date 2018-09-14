d2863d6425d77e6ed94c6c8a645f7f02b8526625
Rodrigo Araujo <alf.rodrigo@gmail.com>
2018-09-13 22:02:16 -0300

    Questão 08 funcionando

--- q08.py
+++ q08.py
@@ -24,7 +24,8 @@ print('Importing finished')
 class PatternGenerator(object):
     def __init__(self, initial_rate, decline_monthly, debug=False):
         self.debug = debug
-
+        self.initial_rate = initial_rate
+        self.decline_monthly = decline_monthly
         X = []
         y = []
         for i in range(6, 366):
@@ -38,37 +39,55 @@ class PatternGenerator(object):
         self.y = np.array(y)
         if self.debug: print("X,y generated")
 
+    def generate_X(self, start, end):
+        X = []
+        for i in range(start, end):
+            tmp = []
+            for j in range(1, 7):
+                tmp.append(i - j)
+            tmp.sort()
+            X.append(tmp)
+        return np.array(X)
+
     def better_plot_data(self, classifiers, labels):
-        cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-        cm = plt.cm.RdBu
         X = np.arange(6, 366)
-        ax1 = plt.subplot(1, 1, 1)
+        X_extra = np.arange(366, 732)
+        y_extra = self.initial_rate * np.exp(self.decline_monthly/30.4 * X_extra)
+        nn_X_extra = self.generate_X(366, 732)
+
+        ax1 = plt.subplot(1, 2, 1)
         ax1.plot(X, self.y)
-        for i, (clf, lbl) in enumerate(zip(classifiers, labels)):
-            if 'DLN' not in lbl:
-                ax1.plot(X, clf.predict(self.X), label=lbl)
-            # else:
-                # test = clf.predict(X)
-                # ax1.scatter(self.X1, self.X2, c=test.T[0], cmap=cm_bright, s=1)
+        ax1.plot(X_extra, y_extra, linestyle='--')
+        for clf, lbl in zip(classifiers, labels):
+            ax1.plot(X, clf.predict(self.X), label=lbl)
+            ax1.plot(X_extra, clf.predict(nn_X_extra), label=lbl+' (previsão)', linestyle='--')
+        ax2 = plt.subplot(1, 2, 2)
+        for clf, lbl in zip(classifiers, labels):
+            ax2.scatter(self.y, clf.predict(self.X), label=lbl+' (entrada)')
+            ax2.scatter(y_extra, clf.predict(nn_X_extra), label=lbl+' (pontos extras)')
+            points = [min(np.hstack((self.y, y_extra)))-0.1, max(np.hstack((self.y, y_extra)))+0.1]
+            ax2.plot(points, points, linestyle='--', color='red', label='m=1')
+            # ax1.plot(X, clf.predict(self.X), label=lbl)
+            # ax1.plot(X_extra, clf.predict(nn_X_extra), label=lbl+' (previsão)', linestyle='--')
         ax1.legend()
+        ax2.legend()
         plt.show()
 
 
 class NeuralNetwork(object):
     def __init__(self, X, y, hidden_layer_sizes=(10,), debug=False):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.1, random_state=27)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=27)
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
         self.clf = MLPRegressor(
             hidden_layer_sizes=hidden_layer_sizes, 
-            max_iter=20000, 
-            # alpha=1e-7,  #1e-3: 0.6
+            alpha=1e-3,
+            activation='logistic',
             solver='lbfgs',
             verbose=debug,
-            random_state=27,
-            # tol=0.000000001
+            random_state=3, # 1 da r2 100% :O
             )
 
     def fit(self):    
@@ -92,32 +111,19 @@ def main():
         debug=debug
         )
 
-    nn0 = NeuralNetwork(pg.X, pg.y, (10,))
-    nn0.fit()
-    if debug:
-        print('NN0 trained')
-        print(nn0.accuracy())
+    # architectures = [(100,100), (300,300,), (400,), (500,), (1000,), (100,100,100), (200,200,200,)]
+    architectures = [(50,)]
+    labels = [f'6x{"x".join(str(i) for i in arch)}x1' for arch in architectures]
+    neural_networks = []
 
-    nn1 = NeuralNetwork(pg.X, pg.y, (100,))
-    nn1.fit()
-    if debug:
-        print('NN1 trained')
-        print(nn1.accuracy())
+    for arch, lbl in zip(architectures,labels):
+        nn = NeuralNetwork(pg.X, pg.y, arch)
+        nn.fit()
+        mse, r2s = nn.accuracy()
+        print(f'{lbl}: mse:{mse} r2_score:{100*r2s:.2f}')
+        neural_networks.append(nn)
 
-    nn2 = NeuralNetwork(pg.X, pg.y, (500,))
-    nn2.fit()
-    if debug:
-        print('NN2 trained')
-        print(nn2.accuracy())
-
-    nn3 = NeuralNetwork(pg.X, pg.y, (1000,))
-    nn3.fit()
-    if debug:
-        print('NN3 trained')
-        print(nn3.accuracy())
-
-
-    pg.better_plot_data([nn0.clf, nn1.clf, nn2.clf, nn3.clf],['6x10x1', '6x100x1', '6x500x1', '6x1000x1'])
+    pg.better_plot_data([nn.clf for nn in neural_networks], labels)
 
 if __name__ == '__main__':
     main()
